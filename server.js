6d30ab1432927ba94e2c43819d406c580cb132a5
Subhadip Mandal <subhadipmandal714@gmail.com>
2025-10-24 19:21:08 +0530

    add user routers

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './Config/db.js';
+import userRouter from './Routers/user.router.js';
 
 dotenv.config();
 connectDB();
@@ -15,6 +16,8 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
