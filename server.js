e560a2c44ec7da312a8f88aaadd5294394ba719c
Subhadip Mandal <subhadipmandal714@gmail.com>
2025-10-24 18:59:47 +0530

    change somthing

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './Config/db.js';
 
 dotenv.config();
+connectDB();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
